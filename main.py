c36d76cc982886e584adf4ceecdd37bcd9ca5d47
sashabusse <sasha.busse@mai.ru>
2021-11-13 00:35:44 +0300

    rework from post corrector to predistorter

--- main.py
+++ main.py
@@ -7,96 +7,113 @@ from raw_adaptive_filters import raw_ls, raw_rls, raw_lms
 
 # << load data >> -----------------------------------------------------------
 Fs = 245.76E6
-d, u = load_data(Fs, fname='data/test4_25p0.mat')
+
+pd_in, pd_out = load_data(Fs, fname='data/test4_25p0.mat')
+pd_err = pd_in-pd_out
+
+# reference signal is remaining error
+d = pd_err
+# input signal for filter is pd_in (input signal of system)
+u = pd_in
+
+# regularization parameters for LS
+# regularization needed, let it be less then d noise level
+reg_sigma = np.sqrt((10 ** (-2)) * Fs)
+r_cond = 0.01
 # ---------------------------------------------------------------------------
 
 # << plot input for this stage >> -------------------------------------------------
 fig_in, ax_in = plt.subplots(1, 1)
 plot_spd([d, u], Fs, ax_in)
 ax_in.set_ylabel("dB")
-ax_in.legend(["d", "u", "err (d-u)"])
+ax_in.legend(["d", "u"])
 ax_in.set_title("input signal")
 # ---------------------------------------------------------------------------
 
-# << auto correlation of input >> ------------------------------------------
-fig_ac, ax_ac = plt.subplots(1, 1)
+# << correlation of input with reference signal >> --------------------------
 rdu = np.correlate(d, u, mode="same")
 rdu_x = np.arange(len(rdu)) - len(rdu)//2
-ax_ac.plot(rdu_x, np.abs(rdu)/np.max(np.abs(rdu)))
-ax_ac.set_title("cross correlation (d, u)")
-ax_ac.set_xlabel("смещение в отсчетах")
-ax_ac.grid(True)
+
+plot_correlation = False
+if plot_correlation:
+    fig_ac, ax_ac = plt.subplots(1, 1)
+    ax_ac.plot(rdu_x, np.abs(rdu)/np.max(np.abs(rdu)))
+    ax_ac.set_title("cross correlation (d, u)")
+    ax_ac.set_xlabel("смещение в отсчетах")
+    ax_ac.grid(True)
 # ---------------------------------------------------------------------------
 
 
 # << compare 3 and 20 order of filter >> ----------------------------------------------------
-compare_3_and_20 = False
+compare_3_and_20 = True
 if compare_3_and_20:
-    # regularization not needed but still let it be less then u noise level
-    reg_sigma = np.sqrt((10**(-4))*Fs)
-    w_ls3, y_ls3 = raw_ls(d, u, ir_len=4, f_delay=1, sigma=reg_sigma, rcond=0.)
-    w_ls20, y_ls20 = raw_ls(d, u, ir_len=21, f_delay=None, sigma=reg_sigma, rcond=0.)
+    w_ls3, y_ls3 = raw_ls(d, u, ir_len=4, f_delay=1, sigma=reg_sigma, rcond=r_cond)
+    w_ls20, y_ls20 = raw_ls(d, u, ir_len=21, f_delay=None, sigma=reg_sigma, rcond=r_cond)
     fig_ls, ax_ls = plt.subplots(1, 1)
-    plot_spd([d, y_ls3, d-y_ls3, y_ls20, d-y_ls20], Fs, ax=ax_ls)
+    plot_spd([d, d-y_ls3, d-y_ls20], Fs, ax=ax_ls)
     plot_afc_db([w_ls3], Fs, ax=ax_ls)
     plot_afc_db([w_ls20], Fs, ax=ax_ls)
     ax_ls.set_ylabel("dB")
-    ax_ls.legend(["d", "y order=3", "err order=3", "y order=20", "err order=20", "afc order=3", "afc order=20"])
+    ax_ls.legend(["d", "remaining err (order=3)", "remaining err (order=20)", "afc (order=3)", "afc (order=20)"])
     ax_ls.set_title("ls optimization results")
+
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(w_ls20.real)
+    ax.plot(w_ls20.imag)
+    ax.set_xlabel("N отсчета")
+    ax.legend(["real part", "imag part"])
+    ax.set_title("ИХ LS (order=20)")
 # ---------------------------------------------------------------------------
 
 
 # << compare 2, 3 order of filter >> ----------------------------------------------------
-compare_2_3 = False
+compare_2_3 = True
 if compare_2_3:
-    reg_sigma = np.sqrt((10**(-4))*Fs)
-    w_ls2, y_ls2 = raw_ls(d, u, ir_len=3, f_delay=0, sigma=reg_sigma, rcond=0.)
-    w_ls3, y_ls3 = raw_ls(d, u, ir_len=4, f_delay=0, sigma=reg_sigma, rcond=0.)
+    w_ls2, y_ls2 = raw_ls(d, u, ir_len=3, f_delay=1, sigma=reg_sigma, rcond=r_cond)
+    w_ls3, y_ls3 = raw_ls(d, u, ir_len=4, f_delay=1, sigma=reg_sigma, rcond=r_cond)
     fig_ls, ax_ls = plt.subplots(1, 1)
-    plot_spd([d, y_ls2, d-y_ls2, y_ls3, d-y_ls3], Fs, ax=ax_ls)
+    plot_spd([d, d-y_ls2, d-y_ls3], Fs, ax=ax_ls)
     plot_afc_db([w_ls2], Fs, ax=ax_ls)
     plot_afc_db([w_ls3], Fs, ax=ax_ls)
     ax_ls.set_ylabel("dB")
-    ax_ls.legend(["d", "y order=2", "err order=2", "y order=3", "err order=3", "afc order=2", "afc order=3"])
+    ax_ls.legend(["d", "remaining err (order=2)", "remaining err (order=3)", "afc (order=2)", "afc (order=3)"])
     ax_ls.set_title("ls optimization results")
 
 # ---------------------------------------------------------------------------
 
 
 # << compare 2, 3 order of filter >> ----------------------------------------------------
-compare_2_1 = False
+compare_2_1 = True
 if compare_2_1:
-    reg_sigma = np.sqrt((10**(-4))*Fs)
-    w_ls2, y_ls2 = raw_ls(d, u, ir_len=3, f_delay=0, sigma=reg_sigma, rcond=0.)
-    w_ls1, y_ls1 = raw_ls(d, u, ir_len=2, f_delay=0, sigma=reg_sigma, rcond=0.)
+    w_ls2, y_ls2 = raw_ls(d, u, ir_len=3, f_delay=1, sigma=reg_sigma, rcond=r_cond)
+    w_ls1, y_ls1 = raw_ls(d, u, ir_len=2, f_delay=1, sigma=reg_sigma, rcond=r_cond)
     fig_ls, ax_ls = plt.subplots(1, 1)
-    plot_spd([d, y_ls2, d-y_ls2, y_ls1, d-y_ls1], Fs, ax=ax_ls)
+    plot_spd([d, d-y_ls2, d-y_ls1], Fs, ax=ax_ls)
     plot_afc_db([w_ls2], Fs, ax=ax_ls)
     plot_afc_db([w_ls1], Fs, ax=ax_ls)
     ax_ls.set_ylabel("dB")
-    ax_ls.legend(["d", "y order=2", "err order=2", "y order=1", "err order=1", "afc order=2", "afc order=1"])
+    ax_ls.legend(["d", "remaining err (order=2)", "remaining err (order=1)", "afc (order=2)", "afc (order=1)"])
     ax_ls.set_title("ls optimization results")
 # ---------------------------------------------------------------------------
 
 # << compare different delay >> ----------------------------------------------------
-compare_delay = False
+compare_delay = True
 if compare_delay:
-    reg_sigma = np.sqrt((10**(-4))*Fs)
-    w_ls_del0, y_ls_del0 = raw_ls(d, u, ir_len=3, f_delay=0, sigma=reg_sigma, rcond=0.)
-    w_ls_del1, y_ls_del1 = raw_ls(d, u, ir_len=3, f_delay=1, sigma=reg_sigma, rcond=0.)
-    w_ls_del2, y_ls_del2 = raw_ls(d, u, ir_len=3, f_delay=2, sigma=reg_sigma, rcond=0.)
+    w_ls_del0, y_ls_del0 = raw_ls(d, u, ir_len=3, f_delay=0, sigma=reg_sigma, rcond=r_cond)
+    w_ls_del1, y_ls_del1 = raw_ls(d, u, ir_len=3, f_delay=1, sigma=reg_sigma, rcond=r_cond)
+    w_ls_del2, y_ls_del2 = raw_ls(d, u, ir_len=3, f_delay=2, sigma=reg_sigma, rcond=r_cond)
 
     fig_ls, ax_ls = plt.subplots(1, 1)
-    plot_spd([d, d-y_ls_del0, d-y_ls_del1], Fs, ax=ax_ls)
+    plot_spd([d, d-y_ls_del0, d-y_ls_del1, d-y_ls_del2], Fs, ax=ax_ls)
     ax_ls.set_ylabel("dB")
-    ax_ls.legend(["d", "err delay=0", "err delay=1"])
+    ax_ls.legend(["d", "err delay=0", "err delay=1", "err delay=2"])
     ax_ls.set_title("ls optimization results (filter order = 2)")
 # ---------------------------------------------------------------------------
 
 # << adapt RLS >> -----------------------------------------------------------
-w_rls, y_rls = raw_rls(d, u, ir_len=3, f_delay=1, sigma_init=rdu[0])
+w_rls, y_rls = raw_rls(d, u, ir_len=3, f_delay=2, sigma_init=rdu[0])
 reg_sigma = np.sqrt((10**(-4))*Fs)
-w_ls, y_ls = raw_ls(d, u, ir_len=3, f_delay=1, sigma=reg_sigma, rcond=0.)
+w_ls, y_ls = raw_ls(d, u, ir_len=3, f_delay=2, sigma=reg_sigma, rcond=0.)
 
 fig_rls, ax_rls = plt.subplots(1, 1)
 plot_spd([d, d-y_rls, d-y_ls], Fs, ax=ax_rls)
@@ -105,7 +122,7 @@ plot_afc_db([w_ls], Fs, ax=ax_rls)
 
 ax_rls.set_ylabel("dB")
 ax_rls.legend(["d", "err rls", "err ls", "afc rls", "afc ls"])
-ax_rls.set_title("ls optimization results (filter order=2, filter delay=1)")
+ax_rls.set_title("ls optimization results (filter order=2, filter delay=2)")
 
 fig_rls_err, ax_rls_err = plt.subplots(1, 1)
 ax_rls_err.plot(signal.lfilter(np.ones(100)/100, 1., np.abs(d-y_rls))[100:])
